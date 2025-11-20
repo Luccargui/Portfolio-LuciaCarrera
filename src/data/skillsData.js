a7803a41eb88afab00ca6e0bb9335017e75856ae
LCG <luciacarreraguillen@gmail.com>
2025-11-20 13:47:41 +0100

    s2

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,30 +1,30 @@
 export const arraySkills = [
   {
-img:"/src/assets/Media/html.png",
+img:"/assets/Media/html.png",
 alt:"html",
   },
     {
-img:"/src/assets/Media/CSS.png",
+img:"/assets/Media/CSS.png",
 alt:"css",
   },
     {
-img:"/src/assets/Media/JavaScript.png",
+img:"/assets/Media/JavaScript.png",
 alt:"javascript",
   },
     {
-img:"/src/assets/Media/NodeJS.png",
+img:"/assets/Media/NodeJS.png",
 alt:"nodejs",
   },
     {
-img:"/src/assets/Media/React.png",
+img:"/assets/Media/React.png",
 alt:"react",
   },
     {
-img:"/src/assets/Media/visualStudioCode.png",
+img:"/assets/Media/visualStudioCode.png",
 alt:"visual-studio-code",
   },
 {
-  img: "/src/assets/Media/GithubDark.png",
+  img: "/assets/Media/GithubDark.png",
   alt: "github",
 },
 ];
