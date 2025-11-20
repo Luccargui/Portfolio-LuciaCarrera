7e797da01d27b2863260f01850f66820d592681a
LCG <luciacarreraguillen@gmail.com>
2025-11-20 13:25:25 +0100

    img control check

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,30 +1,30 @@
 export const arraySkills = [
   {
-img:"../../src/assets/Media/html.png",
+img:"/src/assets/Media/html.png",
 alt:"html",
   },
     {
-img:"../../src/assets/Media/CSS.png",
+img:"/src/assets/Media/CSS.png",
 alt:"css",
   },
     {
-img:"../../src/assets/Media/JavaScript.png",
+img:"/src/assets/Media/JavaScript.png",
 alt:"javascript",
   },
     {
-img:"../../src/assets/Media/NodeJS.png",
+img:"/src/assets/Media/NodeJS.png",
 alt:"nodejs",
   },
     {
-img:"../../src/assets/Media/React.png",
+img:"/src/assets/Media/React.png",
 alt:"react",
   },
     {
-img:"../../src/assets/Media/visualStudioCode.png",
+img:"/src/assets/Media/visualStudioCode.png",
 alt:"visual-studio-code",
   },
 {
-  img: "../../src/assets/Media/GithubDark.png",
+  img: "/src/assets/Media/GithubDark.png",
   alt: "github",
 },
 ];
