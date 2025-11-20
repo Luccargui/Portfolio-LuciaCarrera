5402cc4abc5d8de44bf472526078f7ea00700899
LCG <luciacarreraguillen@gmail.com>
2025-11-20 13:47:11 +0100

    s

--- src/data/option.js
+++ src/data/option.js
@@ -2,17 +2,17 @@ export const arrayOptions = [
   {
     language: "Español",
     inner:"espana",
-img:"/src/assets/Media/espana.png",
+img:"/assets/Media/espana.png",
   },
     {
     language: "English",
     inner:"reino-unido",
-img:"/src/assets/Media/reino-unido.png",
+img:"/assets/Media/reino-unido.png",
   },
     {
     language: "Nederlands",
     inner:"paises-bajos",
-img:"/src/assets/Media/paises-bajos.png",
+img:"/assets/Media/paises-bajos.png",
   }
 ]
 
