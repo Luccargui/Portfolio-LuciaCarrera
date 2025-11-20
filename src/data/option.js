c6f6caceacc0fe157544d7468149f830ae189a75
LCG <luciacarreraguillen@gmail.com>
2025-11-20 12:50:04 +0100

    img problem correction 2

--- src/data/option.js
+++ src/data/option.js
@@ -2,17 +2,17 @@ export const arrayOptions = [
   {
     language: "Español",
     inner:"espana",
-img:"./src/assets/Media/espana.png",
+img:"../../src/assets/Media/espana.png",
   },
     {
     language: "English",
     inner:"reino-unido",
-img:"./src/assets/Media/reino-unido.png",
+img:"../../src/assets/Media/reino-unido.png",
   },
     {
     language: "Nederlands",
     inner:"paises-bajos",
-img:"./src/assets/Media/paises-bajos.png",
+img:"../../src/assets/Media/paises-bajos.png",
   }
 ]
 
--- src/data/presentation.js
+++ src/data/presentation.js
@@ -1,5 +1,5 @@
 export const presentation = {
-  h2:`<img src= "./src/assets/media/saludo-vulcano-unscreen.gif" style="width:20px; 1background-color: black"> ¡Hola! Soy`,
+  h2:`<img src= "../../src/assets/Media/saludo-vulcano-unscreen.gif" style="width:20px; 1background-color: black"> ¡Hola! Soy`,
   h1: `Lucía Carrera`,
   p: ` Full Stack Developper en proceso `,
 }
