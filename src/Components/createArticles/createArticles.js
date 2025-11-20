227742a89fc178b7db5bf6a6b21887710c11578c
LCG <luciacarreraguillen@gmail.com>
2025-11-20 22:19:25 +0100

    crear fondo para artículos con imágenes y efecto mouseenter y mouseleave

--- src/Components/createArticles/createArticles.js
+++ src/Components/createArticles/createArticles.js
@@ -1,48 +1,67 @@
-export const createArticles = (array, classSection) => {
+export const createArticles = (array, classSection) => { 
   const divButtons = document.createElement("div");
   divButtons.className = "divArray";
   const section = document.querySelector(`.${classSection}`);
   section.appendChild(divButtons);
   
   for (const element of array) {
-const article= document.createElement("article");
-divButtons.appendChild(article);
+
+    const article = document.createElement("article");
+    divButtons.appendChild(article);
+    if (element.img) {
+      article.style.backgroundImage = `url(${element.img})`;
+      article.style.backgroundRepeat = "no-repeat";
+      article.style.backgroundSize = "cover";
+      article.style.backgroundPosition = "center";
+    }
+
+    const textContainer = document.createElement("div");
+    textContainer.className = "text-container";
+    textContainer.style.display = "none";
+    article.appendChild(textContainer);
+
 
     const h4 = document.createElement("h4");
-      h4.textContent = `${element.name}`;
-    article.appendChild(h4);
-    
-if(element.title){
-    const title = document.createElement("p");
-            title.textContent = `${element.title}`;
-    article.appendChild(title);
-}
+    h4.textContent = element.name;
+    textContainer.appendChild(h4);
+
+    if (element.title) {
+      const title = document.createElement("p");
+      title.textContent = element.title;
+      textContainer.appendChild(title);
+    }
 
     if (element.duration) {
-  const duration = document.createElement("p");
-      duration.textContent = `${element.duration}`;
-        article.appendChild(duration);
-}
-    if (element.img) {
-  const img = document.createElement("img");
-      img.src = `${element.img}`;
-      img.alt =`${element.title}`;
-        article.appendChild(img);
-}
+      const duration = document.createElement("p");
+      duration.textContent = element.duration;
+      textContainer.appendChild(duration);
+    }
 
     const description = document.createElement("p");
-    description.innerHTML = `${element.description}`;
-    article.appendChild(description);
+    description.innerHTML = element.description;
+    textContainer.appendChild(description);
 
-  if(element.link){
-    const a = document.createElement("a");
-    const button = document.createElement("button");
+    if (element.link) {
+      const a = document.createElement("a");
+      a.href = element.link;
+      const button = document.createElement("button");
+      button.textContent = "Ver";
+      a.appendChild(button);
+      textContainer.appendChild(a);
+    }
 
-    a.href=`${element.link}`;
-    button.textContent= "Ver";
+    article.addEventListener("mouseenter", () => {
+      article.style.backgroundSize = "0%";
+      textContainer.style.display = "flex";
+      textContainer.style.height= "100%";
+      textContainer.style.flexDirection= "column";
+      textContainer.style.alignItems= "center";
+      textContainer.style.justifyContent="space-between";
+    });
 
-    article.appendChild(a);
-    a.appendChild(button);
-  }
+    article.addEventListener("mouseleave", () => {
+      article.style.backgroundSize = "cover"; 
+      textContainer.style.display = "none";
+    });
   }
 };
